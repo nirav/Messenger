3ba028e7816509a093a8f1f60db32c43398c53c9
Nirav Savjani <nsavjani@gmail.com>
2014-10-01 16:43:17 -0700

    Initial Commit

---
+++ Messenger/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Messenger
+//
+//  Created by Nirav Savjani on 10/1/14.
+//  Copyright (c) 2014 Nirav Savjani. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Messenger/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Messenger
+//
+//  Created by Nirav Savjani on 10/1/14.
+//  Copyright (c) 2014 Nirav Savjani. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}
